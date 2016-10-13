ae25eddf0180db8cd73af6b5c3e1f764e0f87447
yuraraco <ningen.yametokina@gmail.com>
2016-10-13 20:25:00 +0900

    20161013

---
+++ testplugin/src/drawoutline_.scala
@@ -0,0 +1,16 @@
+import ij.IJ
+import ij.plugin.filter.ParticleAnalyzer
+
+/**
+  * Created by kawasaki on 2016/10/13.
+  */
+class drawoutline_ {
+  val imp = IJ.getImage
+  val ss = imp.getStackSize
+  val roi = roi
+  val PA = new ParticleAnalyzer()
+
+  for (i <- 1 to ss) {
+
+  }
+}
